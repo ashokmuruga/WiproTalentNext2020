744cad7d12f0cc02cbb4991bd64dc7c24755c2a8
Ashok M <haitwoashok@gmail.com>
2020-05-25 16:47:30 +0530

    25.05.2020

---
+++ Day1a.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+          int n, m;
+        String x = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the Decimal Number:");
+        n = s.nextInt();
+        while(n > 0)
+        {
+            int a = n % 2;
+            x = a + x;
+            n = n / 2;
+        }
+        System.out.println(x);
+    }
+}
