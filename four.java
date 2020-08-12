88cb89968351d09d45758448e1ec1be0dcdc5da8
Ashok M <haitwoashok@gmail.com>
2020-08-12 13:04:01 +0530

    Create four.java

---
+++ four.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class four {
+    public static void main(String args[]) {
+      
+      ArrayList<Double> list = new ArrayList<Double>();
+ 
+        list.add(1.1);
+ 
+        list.add(2.2);
+ 
+        list.add(3.3);
+ 
+        list.add(4.4);
+ 
+        list.add(5.5);
+        list.add(6.6);
+ 
+        System.out.println(list);     
+ 
+        System.out.println("Size Of ArrayList = "+list.size());
+
+        
+        
+    }
+}
