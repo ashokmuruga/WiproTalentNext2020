c29bde0fa230d82029e9c0a162eaee23a02027bf
Ashok M <haitwoashok@gmail.com>
2020-08-12 13:10:28 +0530

    Create five.java

---
+++ five.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class five {
+    public static void main(String args[]) {
+      
+     
+        ArrayList<Double> list = new ArrayList<Double>();
+        
+        
+        System.out.println(list.isEmpty());   
+
+
+        
+        
+    }
+}
