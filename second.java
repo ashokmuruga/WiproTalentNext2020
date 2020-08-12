0667104024d3ec112732721666064187ec75b2dd
Ashok M <haitwoashok@gmail.com>
2020-08-12 12:57:35 +0530

    Create second.java

---
+++ second.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class second {
+    public static void main(String args[]) {
+      
+        
+        ArrayList<String> list = new ArrayList<String>();
+ 
+        //Now 'list' can hold 10 elements (Default Initial Capacity)
+ 
+        list.ensureCapacity(20);
+ 
+        //Now 'list' can hold 20 elements.
+
+    }
+}
