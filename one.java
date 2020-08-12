7e7cd7b25a6edb19ad8dc30418e4f631b50ff89b
Ashok M <haitwoashok@gmail.com>
2020-08-12 12:52:51 +0530

    Create one.java

---
+++ one.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class one {
+    public static void main(String args[]) {
+      
+        ArrayList<Integer> list1 = new ArrayList<Integer>();            //First Method
+ 
+        ArrayList<String> list2 = new ArrayList<String>(20);         //Second Method
+ 
+        ArrayList<Integer> list3 = new ArrayList<Integer>(list1);      //Third Method
+    }
+}
