244c977939ec3dd51d5470a0506c43e802b7a8d3
Ashok M <haitwoashok@gmail.com>
2020-08-12 13:14:49 +0530

    Create six.java

---
+++ six.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class six {
+    public static void main(String args[]) {
+      
+     
+       
+        ArrayList<Double> l = new ArrayList<Double>();
+ 
+        l.add(1.1);
+ 
+        l.add(11.11);
+ 
+        l.add(111.111);
+ 
+        l.add(1111.1111);
+ 
+        //Checking whether list conatins '111.1111'
+        
+        System.out.println(l);
+ 
+        System.out.println(l.contains(111.111));    
+ 
+
+
+        
+        
+    }
+}
