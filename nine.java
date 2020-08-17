1c78ad35f5b8405417ba9ee787ab66757b1bba99
Ashok M <haitwoashok@gmail.com>
2020-08-17 12:56:30 +0530

    Create nine.java

---
+++ nine.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class nine {
+    public static void main(String args[]) {
+      
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+ 
+        list.add(111);
+ 
+        list.add(222);
+ 
+        list.add(333);
+ 
+        list.add(444);
+ 
+        System.out.println(list);     //Output : [111, 222, 333, 444]
+ 
+        //Getting element at index 3
+ 
+        System.out.println(list.get(3));    //Output : 444
+ 
+        //Getting element at index 1
+ 
+        System.out.println(list.get(1));    //Output : 222
+        
+ 
+
+
+    }
+}
